937e321dfa6b5b6056d012d93c1747d5ffbca091
Lefty93 <yongchuseng@gmail.com>
2022-04-02 22:06:38 +0800

    Add ejs file

--- app.js
+++ app.js
@@ -5,15 +5,43 @@ const { application } = require("express");
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
     var today = new Date();
-    if (today.getDay() === 6 || today.getDay() === 0) {
-        res.send("It's the weekend.")
-    } else {
-        res.send("It's the weekday.")
+    var currentDay = today.getDay();
+    var day = "";
+    switch (currentDay) {
+        case 0:
+            day = "Sunday"
+            break;
+        case 1:
+            day = "Monday"
+            break;
+        case 2:
+            day = "Tuesday"
+            break;
+        case 3:
+            day = "Wednesday"
+            break;
+        case 4:
+            day = "Thursday"
+            break;
+        case 5:
+            day = "Friday"
+            break;
+        case 6:
+            day = "Saturday"
+            break;
+        default:
+            console.log("Erro: Current day is equal to: " + currentDay);
+            break;
     }
+
+    res.render("list", { kindOfDay: day });
 });
 
 app.listen(3000, () => {
     console.log("Port is listening at 3000")
+
 })
