20d4108f108d95df2d29bf716e94175f3ae61276
Lefty93 <yongchuseng@gmail.com>
2022-04-04 16:15:01 +0800

    add work list route

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+
 const app = express();
 
 
 
 let items = ["Buy food", "Cook Food", "Eat Food"];
+let workItems = [];
 
 app.set('view engine', 'ejs');
 
@@ -18,14 +20,36 @@ app.get('/', (req, res) => {
     let today = new Date();
     let day = today.toLocaleDateString("en-US", options);
 
-    res.render("list", { kindOfDay: day, newListItems: items });
+    res.render("list", { listTitle: day, newListItems: items });
 });
 
 app.post('/', (req, res) => {
+
     let item = req.body.newItems;
-    items.push(item);
-    res.redirect('/');
+
+    if (req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect('/work');
+    } else {
+        items.push(item);
+        res.redirect('/');
+    }
 })
+
+app.get('/work', (req, res) => {
+    res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+
+app.post('/work', (req, res) => {
+    let item = req.body.newItems;
+    workItems.push(item);
+    res.redirect('/work')
+})
+
+app.get('/about', (req, res) => {
+    res.render("about");
+})
+
 app.listen(3000, () => {
     console.log("Port is listening at 3000")
 
